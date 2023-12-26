443ca7f74462d122d854fae780a5654f8c075ac9
msladic1 <sladic.muris@gmail.com>
2023-12-26 13:06:26 +0100

    Writing more tests

--- SPJProject/src/SPJProject.jl
+++ SPJProject/src/SPJProject.jl
@@ -12,11 +12,11 @@ function initialize_weights(rows, cols, scale=Float32(0.01))
 end
 
 # Initialize a 3x3 matrix of weights with a small random normal distribution
-weights = initialize_weights(5, 5)
+weights = initialize_weights(4069, 11008)
 
 # Convert matrix to QuantMatrix
 quant_matrix, dequant_matrix = convert_to_quant_matrix(weights) 
-print(isapprox(dequant_matrix, weights, atol=1e-3, rtol=1e-3))
+print(isapprox(dequant_matrix, weights, atol=1e-1, rtol=1e-1))
 
 # Display the original and quantized matrices
 # println("Original Matrix:")
--- SPJProject/test/runtests.jl
+++ SPJProject/test/runtests.jl
@@ -10,5 +10,16 @@ weights = initialize_weights(5, 5)
 
 @testset "Check if dequantized matrix is close enough to the original" begin
     q, d = convert_to_quant_matrix(weights)
+    weights2 = initialize_weights(100, 100)
+    q2, d2 = convert_to_quant_matrix(weights2)
     @test isapprox(d, weights, atol=1e-3, rtol=1e-3) == 1
+    @test isapprox(d2, weights2, atol=1e-2, rtol=1e-2) == 1
+end
+
+@testset "Check if quantized values are in valid range" begin
+    q, d = convert_to_quant_matrix(weights)
+    weights2 = initialize_weights(100, 100)
+    q2, d2 = convert_to_quant_matrix(weights2)
+    @test maximum(q) <= 127 && minimum(q) >= -127
+    @test maximum(q2) <= 127 && minimum(q2) >= -127
 end
