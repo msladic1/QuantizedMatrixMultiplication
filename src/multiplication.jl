55af9b1891b3e630996fe30c1e3b4cb79bdf24fc
msladic1 <sladic.muris@gmail.com>
2024-01-24 22:52:47 +0100

    Improving multiplication speed

--- src/multiplication.jl
+++ src/multiplication.jl
@@ -2,16 +2,17 @@ include("quant_functions.jl")
 
 function Base.:*(Q::QuantMatrix{Int8, Float32}, A::Matrix{T}) where T
     C = zeros(Float32, Q.dim[1] , size(A, 2))
-
+    
     axes_q2 = axes(Q, 2)
     axes_a2 = axes(A, 2)
 
     @inbounds for i ∈ axes(Q, 1)
+
+        shared_scale = Q.matrix[i, 1].scale
+
         for j ∈ axes_a2
             Cx = zero(eltype(C))
 
-            shared_scale = Q.matrix[i, 1].scale
-            
             index = 1
 
             for k ∈ axes_q2
